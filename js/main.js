a81a2858c426181bd415cbb537dc22366dd4d15e
Ícaro <icaropereirasun@gmail.com>
2023-09-06 19:54:53 -0400

    doc: remove comment

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const story = document.querySelector(".story");
 
 // valor do dollar no dia 04/09/2023 em relação ao real
 //real / dollar
-const dollar = 1 / 4.92;
+const dollar = 4.92;
 
 function randomValueFromArray(array) {
     const random = Math.floor(Math.random()*array.length);
@@ -15,7 +15,7 @@ function randomValueFromArray(array) {
 
 let stories = [
     "Na final :league, Bob comete um pênalti contra o melhor jogador do mundo, :player, que se prepara para cobrar. Bob se redime ao fazer uma defesa incrível, inspirando o :team a marcar o gol da vitória e conquistar o título, além de um prêmio de R$ 10000 mil reais! Bob se torna o herói do dia.",
-    "O jogador estrela, :player, foi contratado pelo :team por um contrato impressionante de R$ 10000 mil reais, tornando-se o herói dos torcedores. A jogada audaciosa do diretor Bob, que investiu na promessa de sucesso, provou-se acertada. No Maracanã, os torcedores, gratos pelo êxito da contratação, entoavam o nome de Bob, demonstrando sua devoção tanto ao diretor quanto ao talentoso jogador.",
+    "O jogador estrela, :player, foi contratado pelo :team por um contrato impressionante de R$ 10000 mil reais, tornando-se o herói dos torcedores. A jogada audaciosa do diretor Bob, que investiu na promessa de sucesso, provou-se acertada. No Maracanã, os torcedores, gratos pelo êxito da contratação, entoavam o nome de Bob, demonstrando sua devoção tanto ao diretor quanto ao talentoso jogador."
 ];
 
 let players = ["Messi", "CR7", "Ronaldinho"];
@@ -24,7 +24,7 @@ let teams = ["Flamengo", "Manaus FC", "Pysandu"];
 
 let leagues = ["Mundial", "Libertadores", "Copa do Brasil"];
 
-randomize.addEventListener('click', result);
+randomize.addEventListener("click", result);
 
 function result() {
     let newStory = stories;
@@ -36,7 +36,7 @@ function result() {
     }
 
     if(document.getElementById("dollar").checked) {
-        const price = dollar * 10000;
+        const price = 10000 / dollar; 
 
         newStory = newStory.replace("R$ 10000 mil reais", "US$ " + price.toFixed(2) + " mil dólares");
     }
